0dfaec93d5111644a90fc45791826ad08e34dd87
Lakosh <kalimbek.ulan@mail.ru>
2025-08-15 11:13:43 +0500

    Update settings.py

--- vagondar/vagondar/settings.py
+++ vagondar/vagondar/settings.py
@@ -128,7 +128,8 @@ MIDDLEWARE.insert(1, 'whitenoise.middleware.WhiteNoiseMiddleware')
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/5.2/howto/static-files/
 
-STATIC_URL = 'static/'
+STATIC_URL = '/static/'
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 
 
 # Default primary key field type
