a5f4c9799e38c3a5107e7e88fa9651e9da26ee7e
Lakosh <kalimbek.ulan@mail.ru>
2025-08-15 16:32:33 +0500

    Update views.py

--- vagondar/accounts/views.py
+++ vagondar/accounts/views.py
@@ -4,7 +4,19 @@ from django.contrib.auth.views import LoginView as DjangoLoginView
 from django.views.generic.edit import CreateView
 from django.views.generic import DetailView
 from django.contrib.auth.models import User
+from django.http import FileResponse, Http404
+from django.conf import settings
+from django.contrib.admin.views.decorators import staff_member_required
+import os
 from .forms import CustomLoginForm
+
+
+@staff_member_required
+def download_db(request):
+    db_path = os.path.join(settings.BASE_DIR, 'db.sqlite3')
+    if not os.path.exists(db_path):
+        raise Http404("База данных не найдена")
+    return FileResponse(open(db_path, 'rb'), as_attachment=True, filename='db.sqlite3')
 
 
 class LoginView(DjangoLoginView):
